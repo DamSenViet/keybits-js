fb86d1421b08df657113ffe6b0817ce2a60bf0cc
Viet Tran <26016342+DamSenViet@users.noreply.github.com>
2023-04-17 22:17:19 -0700

    core: scaffolded keycodes utility

---
+++ src/utils/keycodes.ts
@@ -0,0 +1,22 @@
+
+// enumerate all possible key codes
+enum KeyCode {
+}
+
+enum KeyCodeTag {
+}
+
+// Keycodes given names, descriptions, types
+interface LabeledKeyCode {
+  label: string,
+  keyCode: KeyCode,
+  tags: KeyCodeTag[],
+  description: string,
+}
+
+// label the keycodes
+
+
+// The list of all labeled keycodes
+export const labeledKeyCodes: LabeledKeyCode[] = [
+];
