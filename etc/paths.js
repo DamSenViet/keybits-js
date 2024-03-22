0b5bf39a3effbc8ed407bfa491c7259047d00340
Viet <26016342+DamSenViet@users.noreply.github.com>
2024-03-22 08:30:10 -0700

    chore: paths cleanup

--- etc/paths.js
+++ etc/paths.js
@@ -9,23 +9,19 @@ const parent = path.resolve(__dirname)
 
 // path to directories
 export const pathToRoot = path.resolve(parent, '..')
-export const pathToConfig = path.resolve(pathToRoot, 'config')
 export const pathToEtc = path.resolve(pathToRoot, 'etc')
 export const pathToSrc = path.resolve(pathToRoot, 'src')
 export const pathToBuild = path.resolve(pathToRoot, 'dist')
 export const pathToNodeModules = path.resolve(pathToRoot, 'node_modules')
 
 // paths to specific files
-export const pathToSrcIndex = path.resolve(pathToSrc, 'index.ts')
-export const pathToPathsJs = path.resolve(pathToEtc, 'paths.js')
+export const pathToEntry = path.resolve(pathToSrc, 'index.ts')
 
 export default {
   pathToRoot,
-  pathToConfig,
   pathToEtc,
   pathToSrc,
   pathToBuild,
   pathToNodeModules,
-  pathToSrcIndex,
-  pathToPathsJs,
+  pathToEntry,
 }
